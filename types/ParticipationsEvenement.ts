b57903513aadb438eb23cd85f0519a60284ca7ad
Jean Carboni <carboni.jean@gmail.com>
2025-08-10 23:10:28 +0200

    feat: remove useless files

--- types/ParticipationsEvenement.ts
+++ types/ParticipationsEvenement.ts
@@ -1,16 +1,1 @@
-export type ParticipationsEvenementFields = keyof ParticipationsEvenement;
-
 export type ParticipationsEvenementReponse = 'present' | 'absent';
-
-export interface ParticipationsEvenement {
-    id: string;
-    evenement_id?: string; // FIXME: can be null
-    utilisateur_id?: string; // FIXME: can be null
-    reponse?: ParticipationsEvenementReponse;
-    besoin_transport?: boolean;
-    transport_valide_par?: string; // FIXME: id
-    commentaire?: string;
-    created_at?: string;
-    lieu_rdv?: string;
-    heure_rdv?: string;
-}
